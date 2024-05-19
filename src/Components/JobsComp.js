4f99e2005047a8ae6e8f8f048f1b5f902c7d0c43
RafiGabizon <rafigabizon11@gmail.com>
2024-05-19 17:35:11 +0300

    opening comp&gifs in recommand comp .

--- src/Components/JobsComp.js
+++ src/Components/JobsComp.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { jobs_ar } from "../data/jobs";
 import '../styles/styles.css';
-import { Link } from "react-router-dom/dist";
 
 export default function JobsComp() {
     return(
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,30 @@
 import React from "react";
-import { jobs_ar } from "../data/jobs";
 import '../styles/styles.css';
 import NavbarTop from '../Components/NavbarTop';
 import NavbarBottom from '../Components/NavbarBottom';
 import JobsComp from "../Components/JobsComp";
+import Recommands from "../Components/RecoComp";
+import OpenComp from "../Components/OpenComp";
 
 export default function Home() {
   return (
     <div className="App">
       <NavbarTop />
+
+      <OpenComp/>
+      <br></br>
+      <h2>
+      : משרות חמות
+      <br></br>
+      </h2>
       <JobsComp/>
+      <br></br>
+      <h2>
+      : הממליצים שלנו
+      <br></br>
+      </h2>
+      <Recommands/>
+      <br></br>
       <NavbarBottom />
     </div>
   );
