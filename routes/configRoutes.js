7758488a9ab231eae1605930b0999e2878392a4f
Almog Kinnan <109092960+AlmogKei@users.noreply.github.com>
2024-09-25 22:29:11 +0300

    Add files via upload

---
+++ routes/configRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const authRoutes = require('./auth');
+
+exports.routesInit = (app) => {
+  app.use('/api/auth', authRoutes);
+  app.use("*", (req, res) => {
+    res.status(404).json({ msg: "Page/endpoint not found, 404" });
+  });
+}
